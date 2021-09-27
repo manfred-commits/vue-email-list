fd55252eb6f2faa71fd37138cdcb30867974bf7e
manfred-commits <manfredipiraino@gmail.com>
2021-09-27 15:45:30 +0200

    added push of 10 responses from the api to emails

--- js/script.js
+++ js/script.js
@@ -11,16 +11,29 @@ const app= new Vue({
 
     el:'#root',
     data:{
-        emailList:[],
+        emails:[],
     },
     mounted(){
-       
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response)=> {            
-        console.log(response);
-        this.emailList.push(response.data.response);
-        console.log(this.emailList);
-        });
+
+        // the while cycle doesn't work, it gives a infinite loop
+        // while(this.emailList.length<10){
+            
+        //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //     .then((response)=> {            
+        //     console.log(response);
+        //     this.emailList.push(response.data.response);
+        //     console.log(this.emailList);
+        //     });
+        // }
+        for(let i=this.emails.length;i<10;i++){
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=> {            
+            console.log(response);
+            this.emails.push(response.data.response);
+            console.log(this.emails);
+            });
+        }
     },
     methods:{
 
