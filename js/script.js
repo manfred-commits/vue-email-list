fef787d67af3cf5ccdd2f4869b39123123f62ec0
manfred-commits <manfredipiraino@gmail.com>
2021-09-27 15:33:52 +0200

    added push into an array of response

--- js/script.js
+++ js/script.js
@@ -14,12 +14,13 @@ const app= new Vue({
         emailList:[],
     },
     mounted(){
-
+       
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response)=> {            
         console.log(response);
-        
-  })
+        this.emailList.push(response.data.response);
+        console.log(this.emailList);
+        });
     },
     methods:{
 
